2fc3819f4582a6ce035c7ca38d8552aa94dbe05e
Your Name <you@example.com>
2023-06-23 00:01:11 +0530

    test

--- handlers/contacts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
- var contactsDataHandler = require('../data/contactsDataHandler');
-
- module.exports = {
-     get: function contacts_get(req, res) {
-         res.json(contactsDataHandler.all())
-     }
- };
